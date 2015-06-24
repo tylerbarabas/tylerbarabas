95d2a6f2fcaa55688bd280072830aabb29754eb2
Tyler Barabas <tylerbarabas@gmail.com>
2015-06-24 15:53:37 -0400

    Wrote logic for avatar spritesheet

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -1,4 +1,4 @@
-define(['json!components/avatar/json/tyler'], function (tyler) {
+define(['lib/Ajax/ajax.js'], function (Ajax) {
     "use strict";
 
     function Avatar() {
@@ -6,11 +6,31 @@ define(['json!components/avatar/json/tyler'], function (tyler) {
 	this.data = null;
 	this.spritesheet = null;
 	this.animation = null;
+	this.stage = null;
+	this.domContainer = null;
+	this.parentContainer = null;
 
     }
 
     Avatar.prototype = {
 	init: function() {
+
+	    this.parentContainer = document.getElementById('sky');
+	    this.domContainer = document.createElement('canvas');
+	    this.parentContainer.appendChild(this.domContainer);
+
+	    Ajax.get('components/avatar/json/tyler.json', function(data){
+		console.log(data);
+		this.data = JSON.parse(data);
+		this.spritesheet = new createjs.SpriteSheet(data);
+		this.animation = new createjs.Sprite(this.spritesheet, "idle");
+		this.stage = new createjs.Stage(this.domContainer);
+
+		this.stage.addChild(this.animation);
+
+		createjs.Ticker.timingMode = create.Ticker.RAF;
+		createjs.Ticker.addEventListener("tick", this.stage);
+	    }).bind(this);
 
 //		this.data = JSON.parse(Tyler);
 //		console.log(this.data);
