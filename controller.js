fa7df7d2d44eea91290c6e32f4f110272e6b3b25
Tyler Barabas <tylerbarabas@gmail.com>
2015-07-14 01:12:43 -0700

    Re-organized controller module

--- controller.js
+++ controller.js
@@ -1,23 +1,36 @@
 require(['components/environment/environment.js', 'components/avatar/avatar.js'],
 function(Environment,Avatar){
 
-    var env = new Environment,
-    currentState = 'day',
-    newState;
-    env.changeState(currentState);
-    setInterval(function(){
-    	console.log('fired!',currentState);
+    function Controller () {
+        
+        this.environment = null;
+        this.environmentState = null;
+        this.avatar = null;
+        
+    }
+    
+    Controller.prototype.init = function () {
+        this.environment = new Environment;
+        this.environmentState = 'day';
+        
+        var newState;
+        
+        this.environment.changeState(this.environmentState);
+        
+        setInterval(function(){
+        	if (this.environmentState == 'day')
+        		newState = 'night';
+        	else
+        		newState = 'day';
+        
+            this.environment.changeState(newState);
+            this.environmentState = newState;
+        }.bind(this),20000);
 
-    	if (currentState == 'day')
-    		newState = 'night';
-    	else 
-    		newState = 'day';
-
-        env.changeState(newState);
-        currentState = newState;
-    },20000);
-
-    var avatar = new Avatar;
-    avatar.init();
-
+        this.avatar = new Avatar;
+        this.avatar.init();
+    };
+    
+    var controller = new Controller;
+    controller.init();
 });
