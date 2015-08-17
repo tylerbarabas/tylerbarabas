1f29fb5a0346591a49ad2194d63222261d358806
Tyler Barabas <tyler.barabas@aofl-642.aofl.local>
2015-08-17 12:35:10 -0700

    Avatar now tweens with arrow keys

--- components/avatar/avatar.js
+++ components/avatar/avatar.js
@@ -9,6 +9,10 @@ define(['lib/Ajax/ajax.js'], function (Ajax) {
 		this.stage = null;
 		this.domContainer = null;
 		this.parentContainer = null;
+		this.currentPosition = null;
+		this.currentDirection = null;
+		this.walking = null;
+		this.moving = null;
 
     }
 
@@ -28,8 +32,16 @@ define(['lib/Ajax/ajax.js'], function (Ajax) {
 
 				this.domContainer.height = this.data.frames.height;
 				this.domContainer.width = this.data.frames.width;
+				this.domContainer.style.top = '65%';
+				this.domContainer.style.left = '50%';
+
+				this.currentPosition = {
+					top: 65,
+					left: 50
+				};
 
 				this.changeSprite('idle');
+				this.walking = false;
 
 				createjs.Ticker.timingMode = createjs.Ticker.RAF;
 				createjs.Ticker.addEventListener("tick", this.stage);
@@ -46,9 +58,11 @@ define(['lib/Ajax/ajax.js'], function (Ajax) {
 				}.bind(this), false);
 
 				document.addEventListener('keyup', function(event) {
+					console.log('keyup!');
 					switch(event.which) {
 						case 37:
 						case 39:
+							console.log('here here');
 							this.walk('idle');
 							break;
 					}
@@ -69,20 +83,62 @@ define(['lib/Ajax/ajax.js'], function (Ajax) {
 		},
 
 		walk: function (direction) {
+			if (this.walking && direction != 'idle') return;
+			var newPosition = {
+				top: this.currentPosition.top,
+				left: this.currentPosition.left
+			};
+
+			this.currentDirection = direction;
+
 			switch(direction) {
 				case 'right':
+					this.walking = true;
 					this.domContainer.style.transform = "rotateY(0deg)";
 					this.changeSprite('walk');
+					
+					newPosition.left += 1;
+					this.moveTo(newPosition);
 					break;
+				
 				case 'left':
+					this.walking = true;
 					this.domContainer.style.transform = "rotateY(180deg)";
 					this.changeSprite('walk');
+
+					newPosition.left -= 1;
+					this.moveTo(newPosition);
 					break;
+
 				case 'idle':
 				default:
+				console.log('GOT HERE');
+					this.walking = false;
 					this.domContainer.style.transform = "rotateY(0deg)";
 					this.changeSprite('idle');
 					break;
+			
+			}
+		},
+
+		moveTo: function(toPos) {
+			if (this.moving) return;
+
+			this.moving = true;
+			this.tween = createjs.Tween.get(this.currentPosition);
+			this.tween.addEventListener('change', function () {
+				this.domContainer.style.top = this.currentPosition.top + '%';
+				this.domContainer.style.left = this.currentPosition.left + '%';
+			}.bind(this));
+
+			this.tween.to(toPos, 60).call(this.moveComplete, [], this);
+		},
+
+		moveComplete: function(){
+			this.moving = false;
+			if (this.walking) {
+				this.walking = false;
+				this.walk(this.currentDirection);
 			}
 		}
     };
